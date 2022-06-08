2d8b086a7c427733ef58798aab5c484434ecb031
Gospodi <gospodijazzet@gmail.com>
2022-06-08 15:36:49 +0300

    add AVAudioSession

--- SimplePong/AppDelegate.swift
+++ SimplePong/AppDelegate.swift
@@ -25,12 +25,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    // MARK: - AVAudioSession
+    
+    private func configureAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(
+                .ambient,
+                mode: .default,
+                options: [.mixWithOthers]
+            )
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Error setting up and activating AVAudioSession: \(error)")
+        }
     }
-
-
 }
 
